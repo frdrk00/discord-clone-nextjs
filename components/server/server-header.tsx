8dc066265de07cfd031b80df20b1fb69a32d7000
frdrk00 <masterkooo@gmail.com>
2023-08-31 21:01:36 +0200

    Invitations

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -1,7 +1,9 @@
 'use client'
 
-import { ServerWithMembersWithProfiles } from '@/types'
 import { MemberRole } from '@prisma/client'
+
+import { ServerWithMembersWithProfiles } from '@/types'
+import { useModal } from '@/hooks/use-modal-store'
 
 import {
   ChevronDown,
@@ -27,6 +29,8 @@ interface ServerHeaderProps {
 }
 
 const ServerHeader = ({ server, role }: ServerHeaderProps) => {
+  const { onOpen } = useModal()
+
   const isAdmin = role === MemberRole.ADMIN
   const isModerator = isAdmin || role === MemberRole.MODERATOR
 
@@ -40,7 +44,9 @@ const ServerHeader = ({ server, role }: ServerHeaderProps) => {
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 text-xs font-medium text-black dark:text-neutral-400 space-y-[2px]">
         {isModerator && (
-          <DropdownMenuItem className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
+          <DropdownMenuItem
+          onClick={() => onOpen("invite", { server})}
+          className="text-indigo-600 dark:text-indigo-400 px-3 py-2 text-sm cursor-pointer">
             Invite People
             <UserIcon className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
