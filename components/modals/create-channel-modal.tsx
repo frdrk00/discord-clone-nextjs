be98189f9ac7554a2766464bd33ef939be6b60cc
frdrk00 <masterkooo@gmail.com>
2023-09-03 16:37:53 +0200

    Server Channel List

--- components/modals/create-channel-modal.tsx
+++ components/modals/create-channel-modal.tsx
@@ -5,6 +5,7 @@ import * as z from 'zod'
 import axios from 'axios'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useParams, useRouter } from 'next/navigation'
+import { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 import { ChannelType } from '@prisma/client'
 
@@ -49,19 +50,28 @@ const formSchema = z.object({
 })
 
 const CreateChannelModal = () => {
-  const { isOpen, onClose, type } = useModal()
+  const { isOpen, onClose, type, data } = useModal()
   const router = useRouter()
   const params = useParams()
 
   const isModalOpen = isOpen && type === 'createChannel'
+  const { channelType } = data
 
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       name: '',
-      type: ChannelType.TEXT,
+      type: channelType || ChannelType.TEXT,
     },
   })
+
+  useEffect(() => {
+    if (channelType) {
+      form.setValue('type', channelType)
+    } else {
+      form.setValue('type', ChannelType.TEXT)
+    }
+  }, [channelType, form])
 
   const isLoading = form.formState.isSubmitting
 
