a954453fd827231321c6539cfbebd81b27d0829b
frdrk00 <masterkooo@gmail.com>
2023-10-03 19:22:47 +0200

    Delete Message

--- components/chat/chat-item.tsx
+++ components/chat/chat-item.tsx
@@ -11,6 +11,7 @@ import { Edit, FileIcon, ShieldAlert, ShieldCheck, Trash } from 'lucide-react'
 import { Member, MemberRole, Profile } from '@prisma/client'
 
 import { cn } from '@/lib/utils'
+import { useModal } from '@/hooks/use-modal-store'
 
 import UserAvatar from '@/components/user-avatar'
 import ActionTooltip from '@/components/action-tooltip'
@@ -56,7 +57,7 @@ const ChatItem = ({
   socketQuery,
 }: ChatItemProps) => {
   const [isEditing, setIsEditing] = useState(false)
-  const [isDeleting, setIsDeleting] = useState(false)
+  const { onOpen } = useModal()
 
   useEffect(() => {
     const handleKeyDown = (e: any) => {
@@ -223,7 +224,15 @@ const ChatItem = ({
             </ActionTooltip>
           )}
           <ActionTooltip label="Delete">
-            <Trash className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition" />
+            <Trash
+              onClick={() =>
+                onOpen('deleteMessage', {
+                  apiUrl: `${socketUrl}/${id}`,
+                  query: socketQuery,
+                })
+              }
+              className="cursor-pointer ml-auto w-4 h-4 text-zinc-500 hover:text-zinc-600 dark:hover:text-zinc-300 transition"
+            />
           </ActionTooltip>
         </div>
       )}
--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -2,16 +2,19 @@
 
 import { useEffect, useState } from 'react'
 
-import CreateServerModal from '@/components/modals/create-server-modal'
-import InviteModal from '@/components/modals/invite-modal'
-import EditServerModal from '@/components/modals/edit-server-modal'
-import MembersModal from '@/components/modals/members-modal'
-import CreateChannelModal from '@/components/modals/create-channel-modal'
-import LeaveServerModal from '@/components/modals/leave-server-modal'
-import DeleteServerModal from '@/components/modals/delete-server-modal'
-import DeleteChannelModal from '@/components/modals/delete-channel-modal'
-import EditChannelModal from '@/components/modals/edit-channel-modal'
-import MessageFileModal from '@/components/modals/message-file-modal'
+import {
+  CreateChannel,
+  CreateServer,
+  DeleteChannel,
+  DeleteMessage,
+  DeleteServer,
+  EditChannel,
+  EditServer,
+  Invite,
+  LeaveServer,
+  Members,
+  MessageFile,
+} from '@/components/modals'
 
 export const ModalProvider = () => {
   const [isMounted, setIsMounted] = useState(false)
@@ -26,16 +29,17 @@ export const ModalProvider = () => {
 
   return (
     <>
-      <CreateServerModal />
-      <InviteModal />
-      <EditServerModal />
-      <MembersModal />
-      <CreateChannelModal />
-      <LeaveServerModal />
-      <DeleteServerModal />
-      <DeleteChannelModal />
-      <EditChannelModal />
-      <MessageFileModal />
+      <CreateServer />
+      <EditServer />
+      <DeleteServer />
+      <CreateChannel />
+      <EditChannel />
+      <DeleteChannel />
+      <Invite />
+      <LeaveServer />
+      <Members />
+      <DeleteMessage />
+      <MessageFile />
     </>
   )
 }
